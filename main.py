f85031d5215e712eb5ba5f42f50ccd3df2e62d59
Denisa Marcisovska <dmarcisovska@gmail.com>
2024-03-23 11:06:12 -0700

    initial playlist organization

---
+++ main.py
@@ -0,0 +1,4 @@
+my_playlist = {
+    "song_1": {"title": "Song Title 1", "artist": "Artist Name 1", "album": "Album Name 1", "duration": "3:45"},
+    "song_2": {"title": "Song Title 2", "artist": "Artist Name 2", "album": "Album Name 2", "duration": "4:30"},
+}
